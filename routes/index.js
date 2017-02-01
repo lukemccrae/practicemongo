a89099d27738ad9bf5e2aef110a5def4c746420a
“lukemccrae” <lukemccrae@gmail.com>
2017-01-31 19:36:19 -0700

    working on put

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const db = require('monk')('localhost/mydb')
+const users = db.get('users')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  users.find({}).then(function(users) {
+    res.json(users)
+  })
+})
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  users.insert(req.body).then(function(result) {
+    res.json(result)
+  })
 });
+
+router.delete('/:id', function(req, res, next) {
+  const id = req.params.id;
+  users.findOneAndDelete({ '_id': req.params.id})
+    .then(function() {
+      res.redirect('/')
+    })
+})
+// 
+// router.put('/:id', function(req, res, next) {
+//   const id = req.params.id;
+//   users.findOneAndUpdate({name: 'foo'}, {name: 'bar'})
+//     .then(function() {
+//       res.redirect('/')
+//     })
+// })
 
 module.exports = router;
